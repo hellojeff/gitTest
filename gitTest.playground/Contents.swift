4540c7a2946e86f3aee80a516c111d4509d9d69e
Jeff Uren <nerfnibbler@gmail.com>
2016-04-30 11:41:13 -0400

    Added vegetables

--- gitTest.playground/Contents.swift
+++ gitTest.playground/Contents.swift
@@ -6,5 +6,10 @@
 let purpose = "This repo is where I will store my playgrounds while I learn Swift"
 
 let tree = "pine"
+let vegetable = "carrot"
 let plant:String = "pothos"
+let numberOfCarrots:Int = 22
+print("I have \(numberOfCarrots) \(vegetable)s")
+
+
 
